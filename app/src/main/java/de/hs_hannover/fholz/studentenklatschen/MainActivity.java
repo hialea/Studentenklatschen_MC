ab32703b6de13c6a2a757186419b74d02dacaf17
hialea <rapshiba100@hotmail.de>
2017-11-21 21:19:02 +0100

    Initial commit

---
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainActivity.java
@@ -0,0 +1,13 @@
+package de.hs_hannover.fholz.studentenklatschen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
