1089b66cdea7f8ce3357e981bf356387d66d786d
Piara <pia.kaschel@gmail.com>
2017-11-23 11:43:24 +0100

    Fragenkatalog Klasse mit Layout

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainActivity.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainActivity.java
@@ -2,14 +2,18 @@ package de.hs_hannover.fholz.studentenklatschen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int x=1;
-        int y=1;
     }
+
 }
