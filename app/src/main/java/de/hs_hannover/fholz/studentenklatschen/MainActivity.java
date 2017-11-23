6b40198bbdc27291a41b3d064351c907be21c3df
efw-kz4-u1 <franziska.holz@stud.hs-hannover.de>
2017-11-23 09:55:10 +0100

    test2

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainActivity.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int x=1;
+        int y=1;
     }
 }
