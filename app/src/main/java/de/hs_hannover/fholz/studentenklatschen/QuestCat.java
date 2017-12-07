fdc770e30177534bb1496440017e14913161aebb
pia <pia.kaschel@gmail.com>
2017-12-07 16:45:54 +0100

    Es geheeeehet nicht

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/QuestCat.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/QuestCat.java
@@ -10,12 +10,17 @@ import android.widget.TextView;
 public class QuestCat extends AppCompatActivity {
 
     private TextView Fragefeld;
+    private TextView klickcount;
     private Button weiterFertig;
-    private int Klick;
+    private int Klick = 1;
     private RadioButton sN;
     private RadioButton uF;
     private RadioButton tP;
     private RadioButton sT;
+    private int countSn = 0;
+    private int countUf = 0;
+    private int countTp = 0;
+    private int countSt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +28,12 @@ public class QuestCat extends AppCompatActivity {
         setContentView(R.layout.activity_quest_cat);
 
         Fragefeld = (TextView) findViewById(R.id.Fragefeld);
+        klickcount = (TextView) findViewById(R.id.Klicks);
         weiterFertig = (Button) findViewById(R.id.button);
         sN = (RadioButton) findViewById(R.id.rBsn);
         uF = (RadioButton) findViewById(R.id.rBuf);
         tP = (RadioButton) findViewById(R.id.rBtp);
         sT = (RadioButton) findViewById(R.id.rBst);
-        Klick = 1;
 
         Fragefeld.setText(R.string.frage1);
         weiterFertig.setText(R.string.weiter);
@@ -41,37 +46,65 @@ public class QuestCat extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                /*clickedRB(v);*/
+                /*boolean checked = ((RadioButton)v).isChecked();
 
-                if (Klick == 1) {
-                    Fragefeld.setText(R.string.frage2);
-                    sN.setText(R.string.aw2sn);
-                    uF.setText(R.string.aw2uf);
-                    tP.setText(R.string.aw2tp);
-                    sT.setText(R.string.aw2st);
-                    Klick++;
-                    weiterFertig.setText(R.string.weiter);
-                } else if (Klick == 2) {
-                    Fragefeld.setText(R.string.frage3);
-                    sN.setText(R.string.aw3sn);
-                    uF.setText(R.string.aw3uf);
-                    tP.setText(R.string.aw3tp);
-                    sT.setText(R.string.aw3st);
-                    Klick++;
-                    weiterFertig.setText(R.string.weiter);
-                } else if (Klick == 3) {
-                    Fragefeld.setText(R.string.frage4);
-                    sN.setText(R.string.aw4sn);
-                    uF.setText(R.string.aw4uf);
-                    tP.setText(R.string.aw4tp);
-                    sT.setText(R.string.aw4st);
-                    Klick++;
-                    weiterFertig.setText(R.string.fertig);
-                } else {
-                    finish();
-                }
+                switch (v.getId()) {
+                    case R.id.rBsn:
+                        if (checked) {
+                            countSn++;
+                        }
+                        break;
+                    case R.id.rBuf:
+                        if (checked) {
+                            countUf++;
+                        }
+                        break;
+                    case R.id.rBtp:
+                        if (checked) {
+                            countTp++;
+                        }
+                        break;
+                    case R.id.rBst:
+                        if (checked) {
+                            countSt++;
+                        }
+                        break;
+                }*/
 
-                    /*case 4:
+                //klickcount.setText("Hallo");
+
+                switch (Klick) {
+                    case 1:
+                        Fragefeld.setText(R.string.frage2);
+                        sN.setText(R.string.aw2sn);
+                        uF.setText(R.string.aw2uf);
+                        tP.setText(R.string.aw2tp);
+                        sT.setText(R.string.aw2st);
+                        Klick++;
+                        weiterFertig.setText(R.string.weiter);
+                        break;
+
+                    case 2:
+                        Fragefeld.setText(R.string.frage3);
+                        sN.setText(R.string.aw3sn);
+                        uF.setText(R.string.aw3uf);
+                        tP.setText(R.string.aw3tp);
+                        sT.setText(R.string.aw3st);
+                        Klick++;
+                        weiterFertig.setText(R.string.weiter);
+                        break;
+
+                    case 3:
+                        Fragefeld.setText(R.string.frage4);
+                        sN.setText(R.string.aw4sn);
+                        uF.setText(R.string.aw4uf);
+                        tP.setText(R.string.aw4tp);
+                        sT.setText(R.string.aw4st);
+                        Klick++;
+                        weiterFertig.setText(R.string.weiter);
+                        break;
+
+                    case 4:
                         Fragefeld.setText(R.string.frage5);
                         sN.setText(R.string.aw5sn);
                         uF.setText(R.string.aw5uf);
@@ -80,19 +113,15 @@ public class QuestCat extends AppCompatActivity {
                         Klick++;
                         weiterFertig.setText(R.string.fertig);
                         break;
-                case 4:
-                break;*/
+                    case 5:
+                        finish();
+                }
             }
         });
 
     }
 
-    private int countSn = 0;
-    private int countUf = 0;
-    private int countTp = 0;
-    private int countSt = 0;
-
-    public void clickedRB(View view) {
+   /* public void clickedRB(View view) {
         boolean checked = ((RadioButton)view).isChecked();
 
         switch (view.getId()) {
@@ -113,5 +142,6 @@ public class QuestCat extends AppCompatActivity {
                     countSt++;
                 break;
         }
-    }
+    }*/
+
 }
