2813713986bb5243569bcadeba33d5086840eb51
lenjasorstner <orange-smile@gmx.de>
2018-01-08 15:22:54 +0100

    navigation bar

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/Menu/Hauptmenue.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/Menu/Hauptmenue.java
@@ -1,10 +1,14 @@
 package de.hs_hannover.fholz.studentenklatschen.Menu;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import de.hs_hannover.fholz.studentenklatschen.R;
 
@@ -14,6 +18,49 @@ public class Hauptmenue extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hauptmenue);
+
+        BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.action_profil:
+                        Toast.makeText(Hauptmenue.this, "Action Profil Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_kampf:
+                        Toast.makeText(Hauptmenue.this, "Action Kampf Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_reise:
+                        Toast.makeText(Hauptmenue.this, "Action Reise Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.action_profil:
+                        Toast.makeText(Hauptmenue.this, "Action Profil Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_kampf:
+                        Toast.makeText(Hauptmenue.this, "Action Kampf Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_reise:
+                        Toast.makeText(Hauptmenue.this, "Action Reise Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
 
         Button profil = (Button)findViewById(R.id.profil);
         Button kampf = (Button)findViewById(R.id.kampf);
