bca5b9ff41c53653381dc6de6818f02c943c5160
pia <pia.kaschel@gmail.com>
2018-01-10 22:07:56 +0100

    Kampf "funktioniert".

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/Fighting.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/Fighting.java
@@ -8,55 +8,43 @@ import android.hardware.SensorManager;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 import de.hs_hannover.fholz.studentenklatschen.R;
 
 public class Fighting extends AppCompatActivity implements SensorEventListener {
 
-    TextView counter, task;
-    private float lastX, lastY, lastZ;
+    TextView counter, task, lp;
     private SensorManager sensorManager;
-    private Sensor accelerometer;
-    private float deltaXMax = 0;
-    private float deltaYMax = 0;
-    private float deltaZMax = 0;
-    private float deltaX = 0;
-    private float deltaY = 0;
-    private float deltaZ = 0;
+    float xAccel, yAccel = 0.0f;
+    float xMax, xMin, yMax, yMin;
+    int lifepoints = 100;
+    Random rn = new Random();
+    int chosenChallenge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fight);
-
-        counter = (TextView) findViewById(R.id.timecount);
-        task = (TextView) findViewById(R.id.challenge);
-
-        task.setVisibility(View.INVISIBLE);
-
+        setContentView(R.layout.activity_fight);;
+        initializeView();
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
-
-            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        } else {
-        }
-
-        countDown(5, counter);
+        task.setText("Follow the instructions in:");
+        countDownPause(5, counter);
 
     }
 
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
     }
 
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener((SensorEventListener) this);
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
     }
 
     @Override
@@ -65,37 +53,13 @@ public class Fighting extends AppCompatActivity implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onSensorChanged(SensorEvent sensorEvent) {
 
-        MaxValues();
-
-        deltaX = Math.abs(lastX - event.values[0]);
-        deltaY = Math.abs(lastY - event.values[1]);
-        deltaZ = Math.abs(lastZ - event.values[2]);
-
-        if (deltaX < 1)
-            deltaX = 0;
-        if (deltaY < 1)
-            deltaY = 0;
-        if (deltaZ < 1)
-            deltaZ = 0;
-
-        lastX = event.values[0];
-        lastY = event.values[1];
-        lastZ = event.values[2];
-
-    }
-
-    public void MaxValues() {
-        if (deltaX > deltaXMax) {
-            deltaXMax = deltaX;
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xAccel = sensorEvent.values[0];
+            yAccel = sensorEvent.values[1];
         }
-        if (deltaY > deltaYMax) {
-            deltaYMax = deltaY;
-        }
-        if (deltaZ > deltaZMax) {
-            deltaZMax = deltaZ;
-        }
+        maxValues();
     }
 
     public void countDown(int Seconds, final TextView displayTime){
@@ -108,18 +72,124 @@ public class Fighting extends AppCompatActivity implements SensorEventListener {
             }
 
             public void onFinish() {
-                displayTime.setVisibility(View.INVISIBLE);
-                task.setVisibility(View.VISIBLE);
-                task.setText(R.string.challengeUpsideDown);
-                challengeUpDown();
-
+                displayTime.setText("0");
+                challenge(chosenChallenge);
+                if (lifepoints > 0) {
+                    countDownPause(3, counter);
+                } else {
+                    task.setText("Gewonnen");
+                }
             }
         }.start();
     }
 
-    public void challengeUpDown (){
-        if (deltaYMax <= (-7)) {
-            task.setText("Prima");
+    public void countDownPause(int Seconds, final TextView displayTime){
+
+        new CountDownTimer(Seconds* 1000+1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                int seconds = (int) (millisUntilFinished / 1000);
+                displayTime.setText(String.format("%d", seconds));
+            }
+
+            public void onFinish() {
+                displayTime.setText("0");
+                challengetxt();
+                countDown(5, counter);
+            }
+        }.start();
+    }
+
+    public void challengetxt() {
+        int rchallenge = rn.nextInt(3);
+        switch (rchallenge) {
+            case 0:
+                task.setText(R.string.challengeUpsideDown);
+                break;
+            case 1:
+                task.setText(R.string.challengeLeft);
+                break;
+            case 2:
+                task.setText(R.string.challengeRight);
+                break;
+            default:
+                task.setText(R.string.challengeUpsideDown);
+                break;
+        }
+        chosenChallenge = rchallenge;
+    }
+
+    public void challenge(int rchallenge) {
+        switch (rchallenge) {
+            case 0:
+                challengeUpDown();
+                break;
+            case 1:
+                challengeLeft();
+                break;
+            case 2:
+                challengeRight();
+                break;
+            default:
+                challengeUpDown();
+                break;
+        }
+    }
+
+    public void challengeUpDown () {
+        task.setText(R.string.challengeUpsideDown);
+        if (yMin < -8) {
+            task.setText("Prima.");
+            lifepoints = lifepoints - 10;
+            lp.setText("lifepoints: " + lifepoints);
+        } else {
+            task.setText("Fail.");
+        }
+        yMin = 0;
+    }
+
+    public void challengeLeft () {
+        task.setText(R.string.challengeLeft);
+        if (xMin < -3) {
+            task.setText("Prima.");
+            lifepoints = lifepoints - 10;
+            lp.setText("lifepoints: " + lifepoints);
+        } else {
+            task.setText("Fail.");
+        }
+        xMin= 0;
+    }
+
+    public void challengeRight () {
+        task.setText(R.string.challengeRight);
+        if (xMax > 3) {
+            task.setText("Prima.");
+            lifepoints = lifepoints - 10;
+            lp.setText("lifepoints: " + lifepoints);
+        } else {
+            task.setText("Fail.");
+        }
+        xMax = 0;
+    }
+
+    public void initializeView() {
+        counter = (TextView) findViewById(R.id.timecount);
+        task = (TextView) findViewById(R.id.challenge);
+        lp = (TextView) findViewById(R.id.lp);
+    }
+
+    public void maxValues() {
+        if (xAccel > xMax) {
+            xMax = xAccel;
+        }
+        if (xAccel < xMin) {
+            xMin = xAccel;
+        }
+        if (yAccel > yMax) {
+            yMax = yAccel;
+        }
+        if (yAccel < yMin) {
+            yMin = yAccel;
         }
     }
 }
