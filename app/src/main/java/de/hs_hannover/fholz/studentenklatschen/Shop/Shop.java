01eadb5562710f708b56ccb98deef22ed2007549
lenjasorstner <orange-smile@gmx.de>
2018-01-10 14:34:42 +0100

    bottom navigation bar die funktionieeeert! (mit Shop)

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/Shop/Shop.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/Shop/Shop.java
@@ -1,7 +1,12 @@
 package de.hs_hannover.fholz.studentenklatschen.Shop;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,6 +15,10 @@ import java.util.TreeMap;
 
 import de.hs_hannover.fholz.studentenklatschen.Datamodel.Affix;
 import de.hs_hannover.fholz.studentenklatschen.Datamodel.Item;
+import de.hs_hannover.fholz.studentenklatschen.MainMenu.Hauptmenue;
+import de.hs_hannover.fholz.studentenklatschen.MainMenu.Kampf;
+import de.hs_hannover.fholz.studentenklatschen.MainMenu.Profil;
+import de.hs_hannover.fholz.studentenklatschen.MainMenu.Vorlesung;
 import de.hs_hannover.fholz.studentenklatschen.R;
 
 import static de.hs_hannover.fholz.studentenklatschen.Datamodel.Attributes.DEFENSE;
@@ -29,6 +38,38 @@ public class Shop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.action_profil:
+                        Intent intent1 = new Intent (Shop.this, Profil.class);
+                        startActivity(intent1);
+                        //Toast.makeText(Hauptmenue.this, "Action Profil Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_kampf:
+                        Intent intent2 = new Intent (Shop.this, Kampf.class);
+                        startActivity(intent2);
+                        //Toast.makeText(Hauptmenue.this, "Action Kampf Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_reise:
+                        Intent intent3 = new Intent (Shop.this, Vorlesung.class);
+                        startActivity(intent3);
+                        //Toast.makeText(Hauptmenue.this, "Action Reise Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_shop:
+                        Intent intent4 = new Intent (Shop.this, Shop.class);
+                        startActivity(intent4);
+                        break;
+                }
+                return false;
+            }
+        });
 
         Random rand = new Random();
         int rndSlot = rand.nextInt(4);
