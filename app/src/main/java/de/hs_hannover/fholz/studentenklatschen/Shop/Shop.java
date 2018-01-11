0f6c35ac7a0d1bb2629a01a1ac4abd9b02105d93
lenjasorstner <orange-smile@gmx.de>
2018-01-12 00:14:08 +0100

    Übersetzung unter values-en

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/Shop/Shop.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/Shop/Shop.java
@@ -38,7 +38,7 @@ public class Shop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
-        BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation_menu);
+        BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation_shop);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -47,19 +47,16 @@ public class Shop extends AppCompatActivity {
                     case R.id.action_profil:
                         Intent intent1 = new Intent (Shop.this, Profil.class);
                         startActivity(intent1);
-                        //Toast.makeText(Hauptmenue.this, "Action Profil Clicked", Toast.LENGTH_SHORT).show();
                         break;
 
                     case R.id.action_kampf:
                         Intent intent2 = new Intent (Shop.this, Kampf.class);
                         startActivity(intent2);
-                        //Toast.makeText(Hauptmenue.this, "Action Kampf Clicked", Toast.LENGTH_SHORT).show();
                         break;
 
                     case R.id.action_reise:
                         Intent intent3 = new Intent (Shop.this, Vorlesung.class);
                         startActivity(intent3);
-                        //Toast.makeText(Hauptmenue.this, "Action Reise Clicked", Toast.LENGTH_SHORT).show();
                         break;
 
                     case R.id.action_shop:
