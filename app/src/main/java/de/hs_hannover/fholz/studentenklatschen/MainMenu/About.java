63c285ffc96ed13bbd1b7bafc795bcc09a2035b6
lenjasorstner <orange-smile@gmx.de>
2018-01-15 22:33:29 +0100

    about_ohne_navigation_bar

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/About.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/About.java
@@ -18,37 +18,5 @@ public class About extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation_about);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    case R.id.action_profil:
-                        Intent intent1 = new Intent (About.this, Profil.class);
-                        startActivity(intent1);
-                        //Toast.makeText(Hauptmenue.this, "Action Profil Clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_fight:
-                        Intent intent2 = new Intent (About.this, GeneratedEnemy.class);
-                        startActivity(intent2);
-                        //Toast.makeText(Hauptmenue.this, "Action Kampf Clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_travel:
-                        Intent intent3 = new Intent (About.this, Travel.class);
-                        startActivity(intent3);
-                        //Toast.makeText(Hauptmenue.this, "Action Reise Clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_shop:
-                        Intent intent4 = new Intent (About.this, Shop.class);
-                        startActivity(intent4);
-                        break;
-                }
-                return false;
-            }
-        });
     }
 }
