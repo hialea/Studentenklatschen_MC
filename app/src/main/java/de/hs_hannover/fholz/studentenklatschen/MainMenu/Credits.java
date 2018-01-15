34997adbd46c1e44a64778d73b34e5a02b057b42
lenjasorstner <orange-smile@gmx.de>
2018-01-15 22:45:33 +0100

    credits beide orientations

--- app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/Credits.java
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/Credits.java
@@ -17,38 +17,5 @@ public class Credits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-
-        BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigation_credits);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
-                {
-                    case R.id.action_profil:
-                        Intent intent1 = new Intent (Credits.this, Profil.class);
-                        startActivity(intent1);
-                        //Toast.makeText(Hauptmenue.this, "Action Profil Clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_fight:
-                        Intent intent2 = new Intent (Credits.this, GeneratedEnemy.class);
-                        startActivity(intent2);
-                        //Toast.makeText(Hauptmenue.this, "Action Kampf Clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_travel:
-                        Intent intent3 = new Intent (Credits.this, Travel.class);
-                        startActivity(intent3);
-                        //Toast.makeText(Hauptmenue.this, "Action Reise Clicked", Toast.LENGTH_SHORT).show();
-                        break;
-
-                    case R.id.action_shop:
-                        Intent intent4 = new Intent (Credits.this, Shop.class);
-                        startActivity(intent4);
-                        break;
-                }
-                return false;
-            }
-        });
     }
 }
