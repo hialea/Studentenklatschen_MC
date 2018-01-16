d2673fdc7e4645bc15948f2311c9476a88752824
hialea <rapshiba100@hotmail.de>
2018-01-16 19:41:46 +0100

    travel

---
+++ app/src/main/java/de/hs_hannover/fholz/studentenklatschen/MainMenu/CharacterProfile.java
@@ -0,0 +1,15 @@
+package de.hs_hannover.fholz.studentenklatschen.MainMenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.hs_hannover.fholz.studentenklatschen.R;
+
+public class CharacterProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_profile);
+    }
+}
